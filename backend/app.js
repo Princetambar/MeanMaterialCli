399c6b3c0d698ebf6af7b4dd5849841d1ca6e26f
amanullahatthar <aniaman2@gmail.com>
2018-06-14 13:53:24 +0530

    Single server setup changes

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ const app = express();
 
 mongoose
   .connect(process.env.MONGO_URL)
+  // .connect('mongodb://localhost:27017/mean-cli')
   .then(() => {
     console.log('Connection to MongoDB successful');
   })
@@ -22,22 +23,13 @@ mongoose
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/images', express.static(path.join('backend/images')));
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
-  );
-  next();
-});
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/', express.static(path.join(__dirname, 'angular')));
 
 app.use('/api/posts', postsRoutes);
 app.use('/api/user', userRoutes);
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, 'angular', 'index.html'));
+});
 
 module.exports = app;
